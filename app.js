5674bb3fd1028cc19924fa4b9a2d228b2780791a
Chandra Sekhar Sirigiri <chandra-sekhar.sirigiri@metlife.com>
2020-04-19 20:32:33 +0800

    Connecting to Mongo DB, Creating schema

--- app.js
+++ app.js
@@ -2,42 +2,88 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHTTP = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+
+const Event = require("./models/event");
 
 const app = express();
+
+const events = [];
 
 app.use(bodyParser.json());
 
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: buildSchema(`    
+    schema: buildSchema(`
+      
+        type Event {
+          _id: String!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+    
         type RootQuery {
-            events: [String!]!
+          events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+          createEvent(eventInput: EventInput): Event
         }
         
         schema {
-            query:RootQuery
-            mutation:RootMutation
+          query: RootQuery
+          mutation: RootMutation
         }
     `),
     rootValue: {
       events: () => {
-        return ["Wedding party", "Marriage Anniversary", "Birthday Function"];
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
       },
-
-      createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: args.eventInput.price,
+          date: new Date(args.eventInput.date),
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
       },
     },
     graphiql: true,
   })
 );
 
-app.listen(3036, () => {
-  console.log("Application running on port 3036...");
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-jjgwl.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => app.listen(3036))
+  .catch(err => console.log(err));
